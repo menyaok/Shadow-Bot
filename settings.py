6f2316bef4aa9bd9a1d384bc613d7bf4cc4127bd
menyaok <81686284+menyaok@users.noreply.github.com>
2021-05-23 15:46:41 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@
 class Settings():
 	
 	def __init__(self):
-		self.Token = {'token': 'ODI2NzUzOTg0Mjg3MDgwNDc4.YGREpg.7ig3fb-p1ARakLjRkB5ZvnAxL5Q','bot': 'Shadow Bot','id': 826753984287080478,'prefix': '!'}
-		self.key = "1ddb09f91397c1090f7f1e1d94ef980255803725028f445eee3c995a3d67fdae"
+		self.Token = {'token': 'CENSORED','bot': 'Shadow Bot','id': CENSORED,'prefix': '!'}
+		self.key = "CENSORED"
